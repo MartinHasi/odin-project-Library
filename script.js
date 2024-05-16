8c032dc7375ea603bf33f4290474de2e27ff37dc
MartinHasi <martinhasieber@gmail.com>
2024-05-16 08:44:13 +0200

    added showBook function added reload function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const dialogButton = document.getElementById('dialog_button');
 const saveButton = document.getElementById('save_data');
 const dialogForm = document.getElementById('input_data');
+const bookContainer = document.getElementById('content_container');
 const titleName = document.querySelector('#book_name');
 const authorName = document.querySelector('#author_name');
 const pageNumber = document.querySelector('#book_pages');
@@ -23,6 +24,7 @@ function Book(title, author, pages, read)
     this.pages = pages;
     this.read = read;
 }
+// Add Book function
 function addBookToLibrary()
 {
     if (myLibrary.length === 0)
@@ -36,12 +38,48 @@ function addBookToLibrary()
     //const book = new Book(titleName.value, authorName.value, pageNumber.value, readBook.value);
     //myLibrary.push(book);
     
-    debugging(titleName.value, authorName.value, pageNumber.value, readBook.value, myLibrary);
-    
+    //debugging(titleName.value, authorName.value, pageNumber.value, readBook.value, myLibrary);
+    reloadBookContent();
+    showBook(myLibrary);
 }
+
+// create function for Book on Homepage
+
+function showBook(array)
+{
+    array.forEach(element => {
+        let container = document.createElement('div');
+        container.classList.add('container');
+        let titleHeader = document.createElement('h3');
+        titleHeader.textContent = element.title;
+        let authorHeader = document.createElement('h3');
+        authorHeader.textContent = element.author;
+        let pagesHeader = document.createElement('h3');
+        pagesHeader.textContent = element.pages;
+        let readHeader = document.createElement('h3');
+        readHeader.textContent = element.read;
+        debugging(titleHeader, authorHeader, pagesHeader, readHeader);
+        container.appendChild(titleHeader);
+        container.appendChild(authorHeader);
+        container.appendChild(pagesHeader);
+        container.appendChild(readHeader);
+        bookContainer.appendChild(container);
+
+    });
+}
+
+// reload function
+function reloadBookContent()
+{
+    bookContainer.replaceChildren();
+}
+
 function debugging(...value)
 {
     console.log(...value);
 }
 
-saveButton.addEventListener('click', addBookToLibrary);
+saveButton.addEventListener('click', () => {
+    addBookToLibrary();
+    dialogForm.close();
+});
